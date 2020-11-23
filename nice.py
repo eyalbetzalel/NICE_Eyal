b0ae9753dfd4d598d44b4229aed15978ec9b2167
Eyal Betzalel <eyalbatzul@gmail.com>
2020-11-23 17:11:31 +0200

    Update nice.py

--- nice.py
+++ nice.py
@@ -165,13 +165,13 @@ class AffineCoupling(nn.Module):
             #z1 = self._first(x)
             #z2 = self._second(x)
             h = self.nonlinearity(z2)
-            scale = torch.exp(self._first(h))
-            shift = self._second(h)
+            scale = torch.exp((h[:, 0::2]))
+            shift = h[:, 1::2]
             ya = (z1 - shift) / scale
             yb = z2
             log_det_J -= torch.log(scale).view(x.shape[0],-1).sum(-1)
-            #y = torch.cat([ya, yb], dim=1)
-            y = _interleave(ya, yb, self.mask_config)
+            y = torch.cat([ya, yb], dim=1)
+            #y = _interleave(ya, yb, self.mask_config)
             
         else:
             
@@ -179,12 +179,12 @@ class AffineCoupling(nn.Module):
             #z1 = self._first(x)
             #z2 = self._second(x)
             h = self.nonlinearity(z2)
-            scale = torch.exp(self._first(h))
-            shift = self._second(h)
+            scale = torch.exp((h[:, 0::2]))
+            shift = h[:, 1::2]
             ya = z1 * scale + shift
             yb = z2
-            #y = torch.cat([ya, yb], dim=1)
-            y = _interleave(ya, yb, self.mask_config)
+            y = torch.cat([ya, yb], dim=1)
+            #y = _interleave(ya, yb, self.mask_config)
             log_det_J += torch.log(torch.abs(scale)).view(x.shape[0],-1).sum(-1)
 
         return y, log_det_J
